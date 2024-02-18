718ce18971526eb772ba6b2e6c8099f9c7ec319f
maikonmuniz <maikon.muniz@yahoo.com.br>
2024-02-17 23:27:45 -0300

    feat: add bcrypt in project

--- src/account/application/create-account-usecases.ts
+++ src/account/application/create-account-usecases.ts
@@ -1,11 +1,12 @@
 import { Account } from "../domain/entities/account.entity";
 import { RepositoryAccount } from "../domain/repository-contract/account.repository";
-
+import { BcryptAdapter } from "../criptography/bcrypt-adapter";
 export class createAccount {
-    constructor(private readonly repositoryAccount: RepositoryAccount) {}
+    constructor(private readonly repositoryAccount: RepositoryAccount, private readonly bcryptAdapter: BcryptAdapter) {}
 
     async execute(input: Input): Promise<Output>{
-        const result = Account.createEntity(input.name, input.email, input.cpf)
+        const password = this.bcryptAdapter.encrypt(input.password)
+        const result = Account.createEntity(input.name, input.email, input.password, input.cpf)
         await this.repositoryAccount.insert(input)
         return result
     }
--- src/account/domain/entities/account.entity.ts
+++ src/account/domain/entities/account.entity.ts
@@ -2,14 +2,16 @@ export class Account {
     name: string;
     email: string;
     cpf: any;
+    password: string;
 
-    private constructor (name: string, email: string, cpf: string){
+    private constructor(name: string, email: string, password: string, cpf: string){
         this.name = name;
         this.email = email;
+        this.password = password;
         this.cpf = cpf;
     }
 
-    static createEntity (name: string, email: string, cpf: string) {
-        return new Account(name, email, cpf);
+    static createEntity (name: string, email: string, password: string, cpf: string) {
+        return new Account(name, email, password, cpf);
     }
 }
